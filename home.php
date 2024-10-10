b635398b1267e104c1d7fba5fe5a3c2f3fb70042
faaanw <akuafwan2@gmail.com>
2024-10-10 12:06:01 +0700

    Templating Admin Peminjama

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+include_once('templates/header.php')
+    ?>
+
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-grey-800">Pinjaman Online</h1>
+
+
+    <?php
+    include_once("templates/footer.php");
+    ?>
